6d6907f941b4cdee8c4ad08338630facfd118655
Ekrash Zahid <mekrashzahid@gmail.com>
2026-06-19 11:09:46 +0500

    assigning the grades

--- taskno1.py
+++ taskno1.py
@@ -5,4 +5,17 @@ name=input("Enter student name:")
 class1=int(input("Enter the name of class:"))
 marks=int(input("Enter the marks of subject physics:"))
 
+if marks>90:
+    print("Grade A")
 
+elif marks>=80 and marks<=90:
+    print("Grade B")
+
+elif marks>70 and marks<80:
+    print("Grade C")
+
+elif marks>60 and marks<70:
+    print("Grade D")
+
+else:
+    print("Grade F")
