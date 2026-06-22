19dcdecb5ff7f5257e1f69cc2ea0622990b386b6
Ekrash Zahid <mekrashzahid@gmail.com>
2026-06-22 15:27:09 +0500

    add and view done

--- Day 3/Student_Recored_Management_system.py
+++ Day 3/Student_Recored_Management_system.py
@@ -16,10 +16,35 @@ def view_student():
         print("No student record found.")
     else:
         print("All Student Records")
-        for student in students:
+        for s in student:
             print("------------------------------")
-            print("Name:",student["name"])
-            print("Roll Number:",student["Roll Number"])
-            print("Age:",student["Age"])
-            print("Course:",student["Course"])
+            print("Name:",s["name"])
+            print("Roll Number:",s["Roll Number"])
+            print("Age:",s["Age"])
+            print("Course:",s["Course"])
     print("Total number of students:",len(student))
+
+def find_student():
+    roll_number=input("Enter the Roll number of student to find:")
+    for s in student:
+        if s["Roll Number"]==roll_number:
+            print("Student found:")
+            print("------------------------------")
+            print("Name:",s["name"])
+            print("Roll Number:",s["Roll Number"])
+            print("Age:",s["Age"])
+            print("Course:",s["Course"])
+            return s
+    return None
+
+def  update_student():
+    found_student=find_student()
+    if found_student:
+        print("Enter new details for the student:")
+        found_student["name"]=input("Enter the name of student:")
+        found_student["Roll Number"]=input("Enter the Roll Number of student:")
+        found_student["Age"]=input("Enter the age of student:")
+        found_student["Course"]=input("Enter the course of student:")
+        print("Student record updated successfully.")
+    else:
+        print("No student record found to update.")
